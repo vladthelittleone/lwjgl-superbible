011d859995cda2f2f331769b95d63c5f606dd39f
Skurishin Vladislav <vskurishin@marathonbet.ru>
2015-04-07 23:00:23 +0300

    First lwjgl shader lesson code.

--- src/main/java/SuperBible1.java
+++ src/main/java/SuperBible1.java
@@ -1,9 +1,13 @@
 import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.*;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
 
 import java.nio.FloatBuffer;
 
+import static java.lang.String.format;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
@@ -11,36 +15,79 @@ import static org.lwjgl.opengl.GL30.*;
 /**
  * @author Skurishin Vladislav
  * @since 06.04.15
- *
- *
+ * <p>
+ * <p>
  * • glCreateShader() creates an empty shader object, ready to accept source code and be compiled.
  * • glShaderSource() hands shader source code to the shader object so that it can keep a copy of it.
  * • glCompileShader() compiles whatever source code is contained in the shader object.
  * • glCreateProgram() creates a program object to which you can attach shader objects.
  * • glAttachShader() attaches a shader object to a program object.
- *￼• glLinkProgram() links all of the shader objects attached to a program object together.
+ * ￼• glLinkProgram() links all of the shader objects attached to a program object together.
  * • glDeleteShader() deletes a shader object. Once a shader has been linked into a program object, the program contains the binary code and the shader is no longer needed.
  */
 public class SuperBible1
 {
     // Source code for vertex shader
     private final String vertexShaderSource =
-                    "#version 430 core                              \n" +
-                    "void main(void)                                \n" +
-                    "{                                              \n" +
-                    "    gl_Position = vec4(0.0, 0.0, 0.5, 1.0);    \n" +
-                    "}                                              \n";
+            "#version 410 core                              \n" +
+            "void main(void)                                \n" +
+            "{                                              \n" +
+            "    gl_Position = vec4(0.0, 0.0, 0.5, 1.0);    \n" +
+            "}                                              \n";
 
     // Source code for fragment shader
     private final String fragmentShaderSource =
-                    "#version 430 core                              \n" +
-                    "out vec4 color;                                \n" +
-                    "void main(void)                                \n" +
-                    "{                                              \n" +
-                    "    color = vec4(0.0, 0.8, 1.0, 1.0);          \n" +
-                    "}                                              \n";
+            "#version 410 core                              \n" +
+            "out vec4 color;                                \n" +
+            "void main(void)                                \n" +
+            "{                                              \n" +
+            "    color = vec4(0.0, 0.8, 1.0, 1.0);          \n" +
+            "}                                              \n";
+
+    private int renderingProgram;
+
+    private int vao;
 
     public void start()
+    {
+        startup();
+
+        render();
+
+        Display.update();
+
+        while (!Display.isCloseRequested())
+        {
+        }
+
+        Display.destroy();
+
+        shutdown();
+    }
+
+    private void render()
+    {
+        long currentTime = System.currentTimeMillis();
+
+        // Simply clear the window with different colors
+        glClearBuffer(GL_COLOR, 0, createClearFloatBuffer(currentTime));
+
+        glPointSize(40f);
+
+        // Use the program object we created earlier for rendering
+        glUseProgram(renderingProgram);
+
+        glDrawArrays(GL_POINTS, 0, 1);
+    }
+
+    private void shutdown()
+    {
+        glDeleteVertexArrays(vao);
+        glDeleteProgram(renderingProgram);
+        glDeleteVertexArrays(vao);
+    }
+
+    private void startup()
     {
         try
         {
@@ -53,20 +100,9 @@ public class SuperBible1
             System.exit(0);
         }
 
-        long currentTime = System.currentTimeMillis();
-
-        // Simply clear the window with different colors
-        glClearBuffer(GL_COLOR, 0, createClearFloatBuffer(currentTime));
-        compileShaders(vertexShaderSource, fragmentShaderSource);
-
-
-        Display.update();
-
-        while (!Display.isCloseRequested())
-        {
-        }
-
-        Display.destroy();
+        renderingProgram = compileShaders(vertexShaderSource, fragmentShaderSource);
+        vao = glGenVertexArrays();
+        glBindVertexArray(vao);
     }
 
     private int compileShaders(String vertexShaderSource, String fragmentShaderSource)
@@ -76,10 +112,22 @@ public class SuperBible1
         glShaderSource(vertexShader, vertexShaderSource);
         glCompileShader(vertexShader);
 
+        if(glGetShaderi(vertexShader, GL_COMPILE_STATUS) == GL_FALSE) {
+            String log = glGetShaderInfoLog(vertexShader, glGetShaderi(vertexShader, GL_INFO_LOG_LENGTH));
+            System.err.println(format("Failure in compiling vertex shader. Error log: %s", log));
+            Display.destroy();
+        }
+
         // Create and compile fragment shader
         int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragmentShader, fragmentShaderSource);
         glCompileShader(fragmentShader);
+
+        if(glGetShaderi(fragmentShader, GL_COMPILE_STATUS) == GL_FALSE) {
+            String log = glGetShaderInfoLog(fragmentShader, glGetShaderi(fragmentShader, GL_INFO_LOG_LENGTH));
+            System.err.println(format("Failure in compiling fragment shader. Error log: %s", log));
+            Display.destroy();
+        }
 
         // Create program, attach shaders to it, and link it
         int program = glCreateProgram();
